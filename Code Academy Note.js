8e105e4dfd9b282f99d6c131ec48c63e0f758f63
Lotus Duff <lotus_duff@selinc.com>
2021-05-07 15:55:08 -0700

    End of night update

--- Code Academy Note.js
+++ Code Academy Note.js
@@ -1,1 +1,19 @@
 console.log(21);
+// This is how you can make a comment. 
+
+
+/* this is also how you can make more 
+comments that will not run and print.
+*/
+
+console.log(5);
+
+// You can also use both examples to print a comment in a line of code and have it not affect the line. See example below.
+
+console.log(/* Comment Inserted*/ 5); //Now it shows that it is still running with a comment here.
+
+/* This kind of comment can be used to make commands within a comment not print 
+console.log(5)
+Not printing ^ see?
+This is cool */
+
