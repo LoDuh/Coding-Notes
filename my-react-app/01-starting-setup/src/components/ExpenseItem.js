7d8c09c6ffa7ebb194bcd6cf732af97841cbe7b7
Lotus Duff <duff8606@vandals.uidaho.edu>
2021-07-09 09:35:36 -0700

    7/9/21

--- my-react-app/01-starting-setup/src/components/ExpenseItem.js
+++ my-react-app/01-starting-setup/src/components/ExpenseItem.js
@@ -9,7 +9,7 @@ function ExpenseItem(props) {
     <div className="expense-item">
       <div>{expenseDate.toISOString()}</div>
       <div className="expense-item__discription">
-        <h2>{expenseTitle}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
